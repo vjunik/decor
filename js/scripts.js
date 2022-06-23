792232d61c6e1727926db71b9b3a116662768734
vjunik <vjunik.du81@gmail.com>
2022-06-23 13:45:48 +0300

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
 
-    $('.owl-carousel').owlCarousel({
+    $('.owl-carousel_1').owlCarousel({
+        loop:true,
+        autoplay: false,
+        autoplayTimeout: 1000,
+        margin:5,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:2
+            }
+        }
+    });
+    $('.owl-carousel_2').owlCarousel({
         loop:true,
         autoplay: false,
         autoplayTimeout: 1000,
